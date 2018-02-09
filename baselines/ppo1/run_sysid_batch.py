1fd65c8dd5531139ec1856e498a0f8068f25b55d
James Preiss <japreiss@usc.edu>
2018-02-08 22:51:45 -0800

    use Dim object

--- baselines/ppo1/run_sysid_batch.py
+++ baselines/ppo1/run_sysid_batch.py
@@ -4,35 +4,34 @@ from baselines import bench
 import gym, logging
 from baselines import logger
 import sysid_batch_policy
+import sysid_direct_batch_policy
 
-def train(env_id, num_timesteps, seed):
+def train(env_id, policy_type, num_timesteps, seed):
     from baselines.ppo1 import mlp_policy, mlp_batch_policy, pposgd_sysid, pposgd_simple, pposgd_batch
 
     U.make_session(num_cpu=1).__enter__()
     set_global_seeds(seed)
 
-    env = gym.make(env_id).env
+    env = gym.make(env_id)
     sysid_dim = int(env.sysid_dim)
 
-    def sysid_policy_fn(name, ob_space, ac_space):
-
-        # user chosen hyperparameters
-        latent_dim = 3
-        n_history = 20
-        alpha_sysid = 0.0
-
-        assert len(ob_space.high.shape) == 1
-        assert len(ac_space.high.shape) == 1
-        total_dim = ob_space.high.shape[0]
-        obs_dim = total_dim - sysid_dim
-
-        return sysid_policy.SysID(name,
-            obs_dim, ac_space.high.shape[0],
-            latent_dim, sysid_dim, n_history, alpha_sysid)
-
     def mlp_policy_fn(name, ob_space, ac_space):
-        return mlp_batch_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
-            hid_size=64, num_hid_layers=2)
+        for space in (ob_space, ac_space):
+            assert isinstance(space, gym.spaces.Box)
+            assert len(space.shape) == 1
+        dim = sysid_batch_policy.Dim(
+            ob = ob_space.shape[0] - sysid_dim,
+            sysid = sysid_dim, # NOTE this is a closure
+            ob_concat = ob_space.shape[0],
+            ac = ac_space.shape[0],
+            embed = 3,
+            agents = env.N,
+            window = 20,
+        )
+        print("dim:", dim)
+
+        return sysid_direct_batch_policy.SysIDDirectPolicy(
+            name=name, dim=dim, hid_size=48, n_hid=2)
 
     def sysid_batch_policy_fn(name, ob_space, ac_space):
         for space in (ob_space, ac_space):
@@ -43,21 +42,30 @@ def train(env_id, num_timesteps, seed):
             sysid = sysid_dim, # NOTE this is a closure
             ob_concat = ob_space.shape[0],
             ac = ac_space.shape[0],
-            embed = 1,
-            agents = 32,
-            window = 100,
+            embed = 3,
+            agents = env.N,
+            window = 64,
         )
-        return sysid_batch_policy.SysIDPolicy(name=name, dim=dim,
-            hid_size=64, n_hid=2)
+        print("dim:", dim)
+
+        return sysid_batch_policy.SysIDPolicy(
+            name=name, dim=dim, hid_size=64, n_hid=2)
+
+    if policy_type == "mlp":
+        policy_fn = mlp_policy_fn
+    elif policy_type == "sysid":
+        policy_fn = sysid_batch_policy_fn
+    else:
+        assert False, "unsupported policy type {} - should be one of ('mlp', 'sysid')".format(policy_type)
 
     #env = bench.Monitor(env, logger.get_dir())
     env.seed(seed)
     gym.logger.setLevel(logging.WARN)
-    pposgd_batch.learn(env, sysid_batch_policy_fn,
+    pposgd_batch.learn(env, policy_fn,
             max_timesteps=num_timesteps,
-            timesteps_per_actorbatch=512,
-            clip_param=0.1, entcoeff=0.0,
-            optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=2048,
+            timesteps_per_actorbatch=256,
+            clip_param=0.2, entcoeff=4.0e-2,
+            optim_epochs=5, optim_stepsize=6e-4, optim_batchsize=512,
             gamma=0.99, lam=0.95, schedule='linear',
         )
     env.close()
@@ -68,9 +76,10 @@ def main():
     parser.add_argument('--env', help='environment ID', default='Hopper-v1')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--num-timesteps', type=int, default=int(1e7))
+    parser.add_argument('--policy', choices=['mlp', 'sysid'])
     args = parser.parse_args()
     logger.configure()
-    train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
+    train(args.env, args.policy, num_timesteps=args.num_timesteps, seed=args.seed)
 
 
 if __name__ == '__main__':
