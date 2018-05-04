46f8f1a9b52a6d0cca2bce5476c392c2995fcfcc
James Preiss <japreiss@usc.edu>
2018-05-04 15:26:56 -0700

    list of per-environment sysid hyperparameters

--- baselines/trpo_mpi/run_trpo_batch.py
+++ baselines/trpo_mpi/run_trpo_batch.py
@@ -11,11 +11,22 @@ from baselines.common.mpi_fork import mpi_fork
 from baselines import bench
 from baselines.trpo_mpi import trpo_batch
 import sys
-#from sysid_batch_policy import SysIDPolicy, Dim
-from sysid_direct_batch_policy import SysIDPolicy, Dim
+from collections import namedtuple
+from sysid_batch_policy import *
+import baselines.common.tf_util as U
 
-def train(env_id, num_timesteps, seed):
-    import baselines.common.tf_util as U
+EnvHypers = namedtuple("EnvHyperparams",
+    "alpha_sysid embed_dim")
+
+env_hypers = {
+    "Reacher-Batch-v1": EnvHypers(alpha_sysid = 0.1, embed_dim = 2),
+    "PointMass-Batch-v0": EnvHypers(alpha_sysid = 0.1, embed_dim = 2),
+}
+
+def train(env_id, num_timesteps, seed, use_embed):
+    assert env_id in env_hypers
+    hypers = env_hypers[env_id]
+
     sess = U.single_threaded_session()
     sess.__enter__()
 
@@ -37,11 +48,12 @@ def train(env_id, num_timesteps, seed):
             sysid = sysid_dim, # NOTE this is a closure
             ob_concat = ob_space.shape[0],
             ac = ac_space.shape[0],
-            embed = 2,
+            embed = hypers.embed_dim,
             agents = env.N,
             window = 20,
         )
-        return SysIDPolicy(name=name, dim=dim, hid_size=32, n_hid=2)
+        return SysIDPolicy(name=name, flavor=EXTRA, dim=dim, hid_size=32, n_hid=2, 
+            alpha_sysid=hypers.alpha_sysid)
 
     #env = bench.Monitor(env, logger.get_dir() and
         #osp.join(logger.get_dir(), str(rank)))
@@ -51,6 +63,17 @@ def train(env_id, num_timesteps, seed):
     trpo_batch.learn(env, sysid_batch_policy_fn, timesteps_per_batch=256, max_kl=0.01, cg_iters=10, cg_damping=0.1,
         max_timesteps=num_timesteps, gamma=0.99, lam=0.98, vf_iters=2, vf_stepsize=1e-3)
     env.close()
+    return pi
+
+def boxprint(lines):
+    maxlen = max(len(s) for s in lines)
+    frame = lambda s, c: c + s + c[::-1]
+    rpad = lambda s: s + " " * (maxlen - len(s))
+    bar = frame("-" * (maxlen + 2), '  *')
+    print(bar)
+    for line in lines:
+        print(frame(rpad(line), '  | '))
+    print(bar)
 
 def main():
     import argparse
@@ -58,9 +81,18 @@ def main():
     parser.add_argument('--env', help='environment ID', default='Hopper-v1')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--num-timesteps', type=int, default=int(1e8))
+    parser.add_argument('--embed', help='use embedding', type=bool, default=False)
     args = parser.parse_args()
     logger.configure()
-    train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
+    boxprint([
+        "starting SysID batch TRPO:",
+        "environment: " + args.env,
+        "using embedding" if args.embed else "NOT using embedding",
+    ])
+    pi = train(args.env, num_timesteps=args.num_timesteps,
+        seed=args.seed, use_embed=args.embed)
+
+
 
 
 if __name__ == '__main__':
